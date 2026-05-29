bbab1ef7839694e4342a26287d3fcaa4f996ba6f
Arthux <rm98329@estudante.fieb.edu.br>
2026-05-29 08:48:43 -0300

    integrei o express validator

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,38 +1,57 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
-router.get("/", (req, res)=>{
-    res.render("pages/index",{"retorno":null,"valores":{"idade":""}});
+router.get("/", (req, res) => {
+
+    res.render("pages/index", { "retorno": null, "valores": { "idade": "" }, "erros": null });
 });
 
+router.post("/classificar",
 
-router.post("/classificar", (req, res)=>{
+    body("idade")
+        .notEmpty().withMessage("A idade é obrigatória!")
+        .isInt({ min: 5, max: 120 }).withMessage("Insira uma idade válida entre 5 e 120 anos"),
+    
+    (req, res) => {
 
-    //recuperar a idade do nadador
-    let idade = parseInt(req.body.idade);
+        const errors = validationResult(req);
+        
+        if (!errors.isEmpty()) {
+ 
+            return res.render("pages/index", {
+                "retorno": null,
+                "valores": req.body,
+                "erros": errors.array()
+            });
+        }
 
-    //manipular os dados -> classificar
-    if(idade >= 5 && idade <=7 ){
-        var categoria = "Infantil A";
-    }else if(idade >= 8 && idade <=10 ){
-        var categoria = "Infantil B";
-    }else if(idade >= 11 && idade <=13 ){
-        var categoria = "Juvenil A";
-    }else if(idade >= 14 && idade <=17 ){
-        var categoria = "Juvenil B";
-    }else if(idade >=18 ){
-        var categoria = "Senior";
-    }else{
-        var categoria = "Idade não classificada";
+
+        let idade = parseInt(req.body.idade);
+        let categoria = "";
+
+        if (idade >= 5 && idade <= 7) {
+            categoria = "Infantil A";
+        } else if (idade >= 8 && idade <= 10) {
+            categoria = "Infantil B";
+        } else if (idade >= 11 && idade <= 13) {
+            categoria = "Juvenil A";
+        } else if (idade >= 14 && idade <= 17) {
+            categoria = "Juvenil B";
+        } else if (idade >= 18) {
+            categoria = "Senior";
+        } else {
+            categoria = "Idade não classificada";
+        }
+
+        let objJson = { "categoria": categoria };
+
+        res.render("pages/index", {
+            "retorno": objJson,
+            "valores": req.body,
+            "erros": null
+        });
     }
-
-    //formatação 
-    let objJson = {"categoria":categoria};
-
-    //envio dos dados para mescalr com o HTML
-    res.render("pages/index",{"retorno":objJson,"valores":{"idade":req.body.idade}})
-
-});
-
+);
 
 module.exports = router;
